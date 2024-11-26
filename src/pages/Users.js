baf464b5e9ee563b338b0d6b1148fc03a69f637b
MariaMVhs <mmvso@iscte-iul.pt>
2024-11-26 17:17:49 +0000

    inicio do projetp

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -3,43 +3,18 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import Row from 'react-bootstrap/Row';
 
 export default function App() {
-    let [users, setBooks] = useState([]);
   
-    const getUsers = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/users', {
-          method: 'GET',
-          headers: {
-          'Content-Type': 'application/json'
-          },
-        });
-        
-        const data = await response.json();
-        console.log(data)
-        setBooks(data.users);
-  
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    }
 
-    useEffect(() => {
-      getUsers();
-    }, []);
+  useEffect(() => {
+    //getUsers();
+  }, []);
 
   return (
     <div className="container pt-5 pb-5">
       <h2>Users Page</h2>
       <CardGroup>
             <Row xs={1} md={2} className="d-flex justify-content-around">
-            {users && users.map((user) => {
-                return (
-                    <UserCard 
-                        key={user._id} 
-                        {...user}
-                    />
-                );
-              })}
+            
             </Row>
         </CardGroup>
     </div>
