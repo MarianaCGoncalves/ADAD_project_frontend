4c9240266d68f2d26aeb77a9fba20caa0c52fd8e
MariaMVhs <mmvso@iscte-iul.pt>
2024-11-26 18:20:54 +0000

    tentativa ponto 1

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from "react";
 import CardGroup from 'react-bootstrap/CardGroup';
 import Row from 'react-bootstrap/Row';
+import UserCard from "../components/UserCard";
+import Button from 'react-bootstrap/Button';
 
 export default function App() {
-    let [users, setBooks] = useState([]);
-  
+    let [users, setUsers] = useState([]);
+    let [page, setPage] = useState([1]);
+    let [totalPgs, setTpgs] = useState([]);
+
     const getUsers = async () => {
       try {
         const response = await fetch('http://localhost:3000/users', {
@@ -16,16 +20,28 @@ export default function App() {
         
         const data = await response.json();
         console.log(data)
-        setBooks(data.users);
-  
+        setUsers(data.users);
+        setTpgs(data.pages.total);
+
       } catch (error) {
         console.error('Error:', error);
       }
     }
 
+
     useEffect(() => {
-      getUsers();
-    }, []);
+      getUsers(page);
+    }, [page]);
+
+    const handlePreviousPg = () => { 
+      if (page > 1) {
+         setPage(page - 1); } 
+    } 
+
+    const handleNextPg = () => { 
+      if (page < totalPgs) {
+         setPage(page + 1); } 
+    }
 
   return (
     <div className="container pt-5 pb-5">
@@ -37,11 +53,16 @@ export default function App() {
                     <UserCard 
                         key={user._id} 
                         {...user}
-                    />
+                    /> 
                 );
               })}
             </Row>
         </CardGroup>
+        <div className="d-flex justify-content-between mt-3"> 
+        <Button onClick={handlePreviousPg} disabled={page == 1}>Página Anterior</Button> 
+        <span>Página {page} de {totalPgs}</span> 
+        <Button onClick={handleNextPg} disabled={page == totalPgs}>Próxima página</Button>
+        </div>
     </div>
   )
 }
